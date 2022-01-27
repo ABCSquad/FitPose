609191ed8251af12c2b0af83cfbc296c175e8ac5
krantheman <akash.akasht.tom@gmail.com>
2022-01-27 14:28:05 +0530

    Set up express and apolloServer

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello World")
+import dotenv from "dotenv";
+import "reflect-metadata";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageProductionDefault,
+} from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
+import { __prod__ } from "./constants";
+import { Context } from "./types";
+import UserResolver from "./resolvers/user";
+
+dotenv.config();
+
+const bootstrap = async () => {
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+    }),
+    context: ({ req, res }): Context => ({ req, res }),
+    plugins: [
+      __prod__
+        ? ApolloServerPluginLandingPageProductionDefault()
+        : ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started on http://localhost:${process.env.PORT}`);
+  });
+};
+
+bootstrap();
