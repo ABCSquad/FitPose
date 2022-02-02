72894b04f0f83ec7d179083dcf7ea4a71b825500
BryanRonad <bryanronad@gmail.com>
2022-02-03 03:54:59 +0530

    Vercel deployment complete

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -2,7 +2,8 @@ import isServer from "./isServer";
 
 const createUrlqlClient = (ssrExchange: any, ctx: any) => {
   let cookie = "";
-  if (isServer()) cookie = ctx.req.headers.cookie;
+  if (isServer()) cookie = ctx?.req?.headers?.cookie;
+
   return {
     url: process.env.NEXT_PUBLIC_API_URL as string,
     fetchOptions: {
