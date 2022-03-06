1d3478960bf0bd0781a392dbf274d41e033383ea
krantheman <akash.akasht.tom@gmail.com>
2022-03-06 13:11:34 +0530

    Redirect from login/signup if already authenticated

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -9,66 +9,74 @@ import InputField from "../components/InputField";
 import { useLoginMutation } from "../generated/graphql";
 import createUrqlClient from "../utils/createUrqlClient";
 import { getErrorMessage } from "../utils/getErrorMessage";
+import { useMeQuery } from "../generated/graphql";
 
 const Login: FC = ({}) => {
   const router = useRouter();
+  const [{ data, fetching }] = useMeQuery();
+  if (data?.me) router.replace("/");
+
   const [, login] = useLoginMutation();
 
-  return (
-    <AuthLayout>
-      <Center h={{ base: "60px", md: 20 }}>
-        <Heading>Log In</Heading>
-      </Center>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await login({ input: values });
-          if (response.error) {
-            const errorMessage = getErrorMessage(response.error);
-            if (errorMessage) setErrors(errorMessage);
-          } else if (response.data?.login._id) router.push("/");
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Box m={3}>
-              <InputField
-                name="email"
-                placeholder="e.g. johndoe@example.com"
-                label="Email"
-              />
-            </Box>
-            <Box m={3}>
-              <InputField
-                name="password"
-                placeholder="********"
-                label="Password"
-                type="password"
-              />
-            </Box>
-            <Box mx={3} mt={5} mb={3}>
-              <Button
-                type="submit"
-                colorScheme="pink"
-                isLoading={isSubmitting}
-                width="100%"
-              >
-                Log In
-              </Button>
-            </Box>
-          </Form>
-        )}
-      </Formik>
-      <Box mx={3}>
-        <Text>
-          Don't have an account?{" "}
-          <NextLink href="/signup">
-            <Link color="blue.600">Sign up.</Link>
-          </NextLink>
-        </Text>
-      </Box>
-    </AuthLayout>
-  );
+  if (!fetching && !data?.me)
+    return (
+      <AuthLayout>
+        <Center h={{ base: "60px", md: 20 }}>
+          <Heading>Log In</Heading>
+        </Center>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await login({ input: values });
+            if (response.error) {
+              const errorMessage = getErrorMessage(response.error);
+              if (errorMessage) setErrors(errorMessage);
+            } else if (response.data?.login._id) router.push("/");
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Box m={3}>
+                <InputField
+                  name="email"
+                  placeholder="e.g. johndoe@example.com"
+                  label="Email"
+                  type="email"
+                />
+              </Box>
+              <Box m={3}>
+                <InputField
+                  name="password"
+                  placeholder="********"
+                  label="Password"
+                  type="password"
+                  autoComplete="on"
+                />
+              </Box>
+              <Box mx={3} mt={5} mb={3}>
+                <Button
+                  type="submit"
+                  colorScheme="pink"
+                  isLoading={isSubmitting}
+                  width="100%"
+                >
+                  Log In
+                </Button>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+        <Box mx={3}>
+          <Text>
+            Don't have an account?{" "}
+            <NextLink href="/signup">
+              <Link color="blue.600">Sign up.</Link>
+            </NextLink>
+          </Text>
+        </Box>
+      </AuthLayout>
+    );
+  return null;
 };
 
 export default withUrqlClient(createUrqlClient)(Login);
--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -9,73 +9,81 @@ import { useRegisterMutation } from "../generated/graphql";
 import createUrqlClient from "../utils/createUrqlClient";
 import { getErrorMessage } from "../utils/getErrorMessage";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 const SignUp: FC = ({}) => {
   const router = useRouter();
+  const [{ data, fetching }] = useMeQuery();
+  if (data?.me) router.replace("/");
+
   const [, register] = useRegisterMutation();
 
-  return (
-    <AuthLayout>
-      <Center h={{ base: "60px", md: 20 }}>
-        <Heading>Sign Up</Heading>
-      </Center>
-      <Formik
-        initialValues={{ email: "", name: "", password: "" }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await register({ input: values });
-          if (response.error) {
-            const errorMessage = getErrorMessage(response.error);
-            if (errorMessage) setErrors(errorMessage);
-          } else if (response.data?.register._id) router.push("/");
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Box m={3}>
-              <InputField
-                name="email"
-                placeholder="e.g. johndoe@example.com"
-                label="Email"
-              />
-            </Box>
-            <Box m={3}>
-              <InputField
-                name="name"
-                placeholder="e.g. John Doe"
-                label="Name"
-              />
-            </Box>
-            <Box m={3}>
-              <InputField
-                name="password"
-                placeholder="********"
-                label="Password"
-                type="password"
-              />
-            </Box>
-            <Box mx={3} mt={5} mb={3}>
-              <Button
-                type="submit"
-                colorScheme="pink"
-                isLoading={isSubmitting}
-                width="100%"
-              >
-                Sign Up
-              </Button>
-            </Box>
-          </Form>
-        )}
-      </Formik>
-      <Box mx={3}>
-        <Text>
-          Already have an account?{" "}
-          <NextLink href="/login">
-            <Link color="blue.600">Log in.</Link>
-          </NextLink>
-        </Text>
-      </Box>
-    </AuthLayout>
-  );
+  if (!fetching && !data?.me)
+    return (
+      <AuthLayout>
+        <Center h={{ base: "60px", md: 20 }}>
+          <Heading>Sign Up</Heading>
+        </Center>
+        <Formik
+          initialValues={{ email: "", name: "", password: "" }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await register({ input: values });
+            if (response.error) {
+              const errorMessage = getErrorMessage(response.error);
+              if (errorMessage) setErrors(errorMessage);
+            } else if (response.data?.register._id) router.push("/");
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Box m={3}>
+                <InputField
+                  name="email"
+                  placeholder="e.g. johndoe@example.com"
+                  label="Email"
+                  type="email"
+                />
+              </Box>
+              <Box m={3}>
+                <InputField
+                  name="name"
+                  placeholder="e.g. John Doe"
+                  label="Name"
+                />
+              </Box>
+              <Box m={3}>
+                <InputField
+                  name="password"
+                  placeholder="********"
+                  label="Password"
+                  type="password"
+                  autoComplete="on"
+                />
+              </Box>
+              <Box mx={3} mt={5} mb={3}>
+                <Button
+                  type="submit"
+                  colorScheme="pink"
+                  isLoading={isSubmitting}
+                  width="100%"
+                >
+                  Sign Up
+                </Button>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+        <Box mx={3}>
+          <Text>
+            Already have an account?{" "}
+            <NextLink href="/login">
+              <Link color="blue.600">Log in.</Link>
+            </NextLink>
+          </Text>
+        </Box>
+      </AuthLayout>
+    );
+  return null;
 };
 
 export default withUrqlClient(createUrqlClient)(SignUp);
