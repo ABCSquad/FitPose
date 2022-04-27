12a2c14f4630a98a3a5e779dbdc79b23ef43236b
BryanRonad <bryanronad@gmail.com>
2022-04-27 22:31:29 +0530

    Landmark custom

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,209 +1,229 @@
 import React, { FC, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 import {
-	Pose,
-	POSE_CONNECTIONS,
-	POSE_LANDMARKS_LEFT,
-	POSE_LANDMARKS_RIGHT,
-	POSE_LANDMARKS_NEUTRAL,
+  Pose,
+  POSE_CONNECTIONS,
+  POSE_LANDMARKS_LEFT,
+  POSE_LANDMARKS_RIGHT,
+  POSE_LANDMARKS_NEUTRAL,
 } from "@mediapipe/pose";
 import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
 import { Camera } from "@mediapipe/camera_utils";
 import {
-	AspectRatio,
-	Badge,
-	HStack,
-	Text,
-	VisuallyHidden,
+  AspectRatio,
+  Badge,
+  HStack,
+  Text,
+  VisuallyHidden,
 } from "@chakra-ui/react";
 import Core from "../core/core";
 import { useApp } from "../contexts/AppContext";
 import { LandmarkGrid } from "@mediapipe/control_utils_3d";
 
 const Canvas: FC = () => {
-	const { blurState, FPS, setBlurState, setMetaData, setRepCounter, setFPS } =
-		useApp();
+  const { blurState, FPS, setBlurState, setMetaData, setRepCounter, setFPS } =
+    useApp();
 
-	const videoRef = useRef<Webcam | null>(null);
-	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const videoRef = useRef<Webcam | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-	if (canvasRef.current) {
-		canvasRef.current.height = 720;
-		canvasRef.current.width = 1280;
-	}
+  const excludedLandmarkSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const excludedConnectionSet = [
+    [0, 1],
+    [1, 2],
+    [2, 3],
+    [3, 7],
+    [0, 4],
+    [4, 5],
+    [5, 6],
+    [6, 8],
+    [9, 10],
+  ];
 
-	const landmarkRef = useRef<HTMLDivElement | null>(null);
-	let coreInstance: Core;
-	let canvasCtx: CanvasRenderingContext2D | null;
-	let lastFrameTime = performance.now();
+  if (canvasRef.current) {
+    canvasRef.current.height = 720;
+    canvasRef.current.width = 1280;
+  }
 
-	// const grid = new LandmarkGrid(landmarkRef.current!, {
-	//   connectionColor: 0xcccccc,
-	//   definedColors: [
-	//     { name: "LEFT", value: 0xffa500 },
-	//     { name: "RIGHT", value: 0x00ffff },
-	//   ],
-	//   range: 2,
-	//   fitToGrid: true,
-	//   labelSuffix: "m",
-	//   landmarkSize: 2,
-	//   numCellsPerAxis: 4,
-	//   showHidden: false,
-	//   centered: true,
-	// });
+  const landmarkRef = useRef<HTMLDivElement | null>(null);
+  let coreInstance: Core;
+  let canvasCtx: CanvasRenderingContext2D | null;
+  let lastFrameTime = performance.now();
 
-	const videoConstraints = {
-		width: 1280,
-		height: 720,
-	};
+  // const grid = new LandmarkGrid(landmarkRef.current!, {
+  //   connectionColor: 0xcccccc,
+  //   definedColors: [
+  //     { name: "LEFT", value: 0xffa500 },
+  //     { name: "RIGHT", value: 0x00ffff },
+  //   ],
+  //   range: 2,
+  //   fitToGrid: true,
+  //   labelSuffix: "m",
+  //   landmarkSize: 2,
+  //   numCellsPerAxis: 4,
+  //   showHidden: false,
+  //   centered: true,
+  // });
 
-	/* Scale to fill with aspect ratio */
-	// const scaleToFill = (ctx: CanvasRenderingContext2D) => {
-	// 	let canvasDim = {
-	// 		w: canvasRef.current!.width,
-	// 		h: canvasRef.current!.height,
-	// 	};
-	// 	// getting the scale
-	// 	scale = Math.max(
-	// 		window.innerWidth / canvasDim.w,
-	// 		window.innerHeight / canvasDim.h
-	// 	);
-	// 	// get top left pos of window
-	// 	let x = window.innerWidth / 2 - (canvasDim.w / 2) * scale;
-	// 	let y = window.innerHeight / 2 - (canvasDim.h / 2) * scale;
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+  };
 
-	// 	ctx.setTransform(scale, 0, 0, scale, x, y);
-	// };
+  /* Scale to fill with aspect ratio */
+  // const scaleToFill = (ctx: CanvasRenderingContext2D) => {
+  // 	let canvasDim = {
+  // 		w: canvasRef.current!.width,
+  // 		h: canvasRef.current!.height,
+  // 	};
+  // 	// getting the scale
+  // 	scale = Math.max(
+  // 		window.innerWidth / canvasDim.w,
+  // 		window.innerHeight / canvasDim.h
+  // 	);
+  // 	// get top left pos of window
+  // 	let x = window.innerWidth / 2 - (canvasDim.w / 2) * scale;
+  // 	let y = window.innerHeight / 2 - (canvasDim.h / 2) * scale;
 
-	// useEffect(() => {
-	//   console.log(blurState);
-	// }, [blurState]);
+  // 	ctx.setTransform(scale, 0, 0, scale, x, y);
+  // };
 
-	const onResults = (results: any) => {
-		setFPS(1 / ((performance.now() - lastFrameTime) / 1000));
-		lastFrameTime = performance.now();
-		canvasCtx = canvasRef.current!.getContext("2d");
-		// scaleToFill(canvasCtx!);
-		setBlurState(coreInstance.blur());
-		const getValue: any = coreInstance.update(results.poseLandmarks);
-		if (getValue != undefined) setRepCounter(getValue.repObj.count);
-		setMetaData(getValue);
+  // useEffect(() => {
+  //   console.log(blurState);
+  // }, [blurState]);
 
-		if (canvasCtx && canvasRef.current) {
-			canvasCtx.save();
-			canvasCtx.clearRect(
-				0,
-				0,
-				canvasRef.current.width,
-				canvasRef.current.height
-			);
-			canvasCtx.drawImage(
-				results.image,
-				0,
-				0,
-				canvasRef.current.width,
-				canvasRef.current.height
-			);
-			if (results.poseLandmarks) {
-				drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-					visibilityMin: 0.65,
-					color: "white",
-				});
-				drawLandmarks(
-					canvasCtx,
-					Object.values(POSE_LANDMARKS_LEFT).map(
-						(index) => results.poseLandmarks[index]
-					),
-					{ visibilityMin: 0.65, color: "white", fillColor: "rgb(255,138,0)" }
-				);
-				drawLandmarks(
-					canvasCtx,
-					Object.values(POSE_LANDMARKS_RIGHT).map(
-						(index) => results.poseLandmarks[index]
-					),
-					{ visibilityMin: 0.65, color: "white", fillColor: "rgb(0,217,231)" }
-				);
-				drawLandmarks(
-					canvasCtx,
-					Object.values(POSE_LANDMARKS_NEUTRAL).map(
-						(index) => results.poseLandmarks[index]
-					),
-					{ visibilityMin: 0.65, color: "white", fillColor: "white" }
-				);
-			}
-			canvasCtx.restore();
-		}
-	};
+  const onResults = (results: any) => {
+    setFPS(1 / ((performance.now() - lastFrameTime) / 1000));
+    lastFrameTime = performance.now();
+    canvasCtx = canvasRef.current!.getContext("2d");
+    // scaleToFill(canvasCtx!);
+    setBlurState(coreInstance.blur());
+    const getValue: any = coreInstance.update(results.poseLandmarks);
+    if (getValue != undefined) setRepCounter(getValue.repObj.count);
+    setMetaData(getValue);
 
-	useEffect(() => {
-		const exerciseArray = [
-			{ name: "ohp", keypoints: [23, 24, 11, 12, 13, 14, 15, 16], reps: 10 },
-		];
-		coreInstance = new Core(exerciseArray);
+    if (canvasCtx && canvasRef.current) {
+      canvasCtx.save();
+      canvasCtx.clearRect(
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+      canvasCtx.drawImage(
+        results.image,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+      if (results.poseLandmarks) {
+        drawConnectors(
+          canvasCtx,
+          results.poseLandmarks,
+          POSE_CONNECTIONS.filter((n) => !excludedConnectionSet.includes(n)),
+          {
+            visibilityMin: 0.65,
+            color: "white",
+          }
+        );
+        drawLandmarks(
+          canvasCtx,
+          Object.values(POSE_LANDMARKS_LEFT).map((index) => {
+            if (!excludedLandmarkSet.includes(index))
+              return results.poseLandmarks[index];
+          }),
+          { visibilityMin: 0.65, color: "white", fillColor: "rgb(255,138,0)" }
+        );
+        drawLandmarks(
+          canvasCtx,
+          Object.values(POSE_LANDMARKS_RIGHT).map((index) => {
+            if (!excludedLandmarkSet.includes(index))
+              return results.poseLandmarks[index];
+          }),
+          { visibilityMin: 0.65, color: "white", fillColor: "rgb(0,217,231)" }
+        );
+        // drawLandmarks(
+        //   canvasCtx,
+        //   Object.values(POSE_LANDMARKS_NEUTRAL).map(
+        //     (index) => results.poseLandmarks[index]
+        //   ),
+        //   { visibilityMin: 0.65, color: "white", fillColor: "white" }
+        // );
+      }
+      canvasCtx.restore();
+    }
+  };
 
-		const pose = new Pose({
-			locateFile: (file) => {
-				return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-			},
-		});
-		pose.setOptions({
-			modelComplexity: 1,
-			selfieMode: true,
-			smoothLandmarks: true,
-			enableSegmentation: false,
-			smoothSegmentation: true,
-			minDetectionConfidence: 0.6,
-			minTrackingConfidence: 0.3,
-		});
-		pose.onResults(onResults);
-		if (typeof videoRef.current !== "undefined" && videoRef.current !== null) {
-			const camera = new Camera(videoRef.current.video!, {
-				onFrame: async () => {
-					await pose.send({ image: videoRef.current!.video! });
-				},
-				width: 1280,
-				height: 720,
-			});
-			camera.start();
-		}
-	}, []);
+  useEffect(() => {
+    const exerciseArray = [
+      { name: "ohp", keypoints: [23, 24, 11, 12, 13, 14, 15, 16], reps: 10 },
+    ];
+    coreInstance = new Core(exerciseArray);
 
-	return (
-		<div className="container">
-			<VisuallyHidden>
-				<Webcam
-					audio={false}
-					height={720}
-					ref={videoRef}
-					screenshotFormat="image/jpeg"
-					width={1280}
-					videoConstraints={videoConstraints}
-					// onUserMedia={() => {
-					// 	console.log("videoRef.current", videoRef.current);
-					// }}
-				/>
-			</VisuallyHidden>
-			<div className="overlay-fps">
-				<Badge colorScheme="purple" w={16}>
-					<HStack color="green">
-						<Text>FPS :</Text>
-						<Text color="green" fontSize="lg">
-							{FPS != 0 ? ` ${FPS.toPrecision(2)}` : null}
-						</Text>
-					</HStack>
-				</Badge>
-			</div>
-			<canvas
-				className="content"
-				ref={canvasRef}
-				// style={{
-				//   height: "100%",
-				//   width: "100%",
-				// }}
-			/>
-			<div ref={landmarkRef}></div>
-		</div>
-	);
+    const pose = new Pose({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+      },
+    });
+    pose.setOptions({
+      modelComplexity: 1,
+      selfieMode: true,
+      smoothLandmarks: true,
+      enableSegmentation: false,
+      smoothSegmentation: true,
+      minDetectionConfidence: 0.6,
+      minTrackingConfidence: 0.3,
+    });
+    pose.onResults(onResults);
+    if (typeof videoRef.current !== "undefined" && videoRef.current !== null) {
+      const camera = new Camera(videoRef.current.video!, {
+        onFrame: async () => {
+          await pose.send({ image: videoRef.current!.video! });
+        },
+        width: 1280,
+        height: 720,
+      });
+      camera.start();
+    }
+  }, []);
+
+  return (
+    <div className="container">
+      <VisuallyHidden>
+        <Webcam
+          audio={false}
+          height={720}
+          ref={videoRef}
+          screenshotFormat="image/jpeg"
+          width={1280}
+          videoConstraints={videoConstraints}
+          // onUserMedia={() => {
+          // 	console.log("videoRef.current", videoRef.current);
+          // }}
+        />
+      </VisuallyHidden>
+      <div className="overlay-fps">
+        <Badge colorScheme="purple" w={16}>
+          <HStack color="green">
+            <Text>FPS :</Text>
+            <Text color="green" fontSize="lg">
+              {FPS != 0 ? ` ${FPS.toPrecision(2)}` : null}
+            </Text>
+          </HStack>
+        </Badge>
+      </div>
+      <canvas
+        className="content"
+        ref={canvasRef}
+        // style={{
+        //   height: "100%",
+        //   width: "100%",
+        // }}
+      />
+      <div ref={landmarkRef}></div>
+    </div>
+  );
 };
 
 export default Canvas;
