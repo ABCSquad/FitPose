1543f8d9cb8061f2e262e4dfbf6cc912f032bb49
Cris10Lobo <cris10lobo@gmail.com>
2022-05-06 22:29:19 +0530

    Updated HumanBody to our needs

--- client/src/components/widgets/AppHuman/AppHuman.tsx
+++ client/src/components/widgets/AppHuman/AppHuman.tsx
@@ -1,7 +1,63 @@
 import { Box, Button } from "@chakra-ui/react";
 import React, { FC, useState } from "react";
+import { HumanBody } from "./HumanBody";
 
 const AppHuman: FC = () => {
+	const [bodyState, setBodyState] = useState({
+		head: {
+			show: true,
+			selected: false,
+		},
+		left_shoulder: {
+			show: true,
+			selected: false,
+		},
+		right_shoulder: {
+			show: true,
+			selected: false,
+		},
+		left_arm: {
+			show: true,
+			selected: false,
+		},
+		right_arm: {
+			show: true,
+			selected: false,
+		},
+		chest: {
+			show: true,
+			selected: false,
+		},
+		stomach: {
+			show: true,
+			selected: false,
+		},
+		left_leg: {
+			show: true,
+			selected: false,
+		},
+		right_leg: {
+			show: true,
+			selected: false,
+		},
+		left_hand: {
+			show: true,
+			selected: false,
+		},
+		right_hand: {
+			show: true,
+			selected: false,
+		},
+		left_foot: {
+			show: true,
+			selected: false,
+		},
+		right_foot: {
+			show: true,
+			selected: false,
+		},
+	});
+
 	return (
 		<Box
 			mb={3}
@@ -11,7 +67,22 @@ const AppHuman: FC = () => {
 			borderRadius="30px"
 			borderColor="InactiveBorder"
 			bg="white"
-		></Box>
+		>
+			<HumanBody partsInput={bodyState} />
+			<Button
+				onClick={() =>
+					setBodyState((prev) => ({
+						...prev,
+						right_foot: {
+							show: true,
+							selected: !false,
+						},
+					}))
+				}
+			>
+				Hello
+			</Button>
+		</Box>
 	);
 };
 
